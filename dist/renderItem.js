08939a7415ba51aa25b81e1918c9f8de208376b8
Prajwal Lokhande <prajwallokhande27@gmail.com>
2025-08-16 16:25:40 +0530

    Task 4 Submittion

---
+++ dist/renderItem.js
@@ -0,0 +1,35 @@
+import renderModal from "./modal.js";
+export default function renderItem(index, data) {
+    const item = document.querySelector('.item');
+    const modal = document.getElementById('modal');
+    item.innerHTML = '';
+    const currentItem = data[index];
+    const itemTitle = document.createElement('h3');
+    itemTitle.classList.add('item-title');
+    itemTitle.textContent = currentItem.title;
+    item.appendChild(itemTitle);
+    const itemImageContainer = document.createElement('div');
+    itemImageContainer.classList.add('item-image-container');
+    const itemImage = document.createElement('img');
+    itemImage.classList.add('item-image');
+    itemImage.src = currentItem.imageURL;
+    itemImageContainer.appendChild(itemImage);
+    const itemImageCaption = document.createElement('div');
+    itemImageCaption.classList.add('item-image-caption');
+    itemImageCaption.textContent = currentItem.category;
+    itemImageContainer.appendChild(itemImageCaption);
+    item.appendChild(itemImageContainer);
+    const itemDescription = document.createElement('div');
+    itemDescription.classList.add('item-description');
+    itemDescription.textContent = currentItem.description;
+    item.appendChild(itemDescription);
+    const loadMoreBtn = document.createElement('button');
+    loadMoreBtn.textContent = 'Load More';
+    loadMoreBtn.classList.add('load-more-btn');
+    loadMoreBtn.addEventListener('click', () => {
+        modal.classList.remove('d-none');
+        modal.classList.add('d-flex');
+        renderModal(index, data);
+    });
+    item.appendChild(loadMoreBtn);
+}
